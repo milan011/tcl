41fb7c6fe22bbc88e480c68df32a4389d53905ab
wcgInTaoCheLe <milan015@sina.com>
2016-11-20 16:55:21 +0800

    添加用户验证规则修改

--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -29,9 +29,8 @@ class StoreUserRequest extends Request
             'password'              => 'required|min:6|confirmed',
             'password_confirmation' => 'required|min:6',
             'telephone'             => 'required|phone_number',
-            // 'telephone'             => 'phone_number',
             'email'                 => 'email|unique:tcl_user',
-            'role_id'               => 'required',
+            'role_id'               => 'required|numeric|min:1',
             'shop_id'               => 'required'
         ];
     }
@@ -48,11 +47,14 @@ class StoreUserRequest extends Request
             'nick_name.unique'                => '实际姓名重复',
             'nick_name.required'              => '请输入实际姓名',
             'password.required'               => '请输入密码',
+            'password.min'                    => '密码不能少于6位',
             'password.confirmed'              => '两次输入的密码不一致',
             'password_confirmation.required'  => '请确认密码',
+            'password_confirmation.min'       => '确认密码不能少于6位',
             'telephone.required'              => '请输入手机号码',
             'telephone.phone_number'          => '手机号码无效',
             'role_id.required'                => '请选择角色',
+            'role_id.min'                     => '请选择角色',
             'shop_id.required'                => '请选择门店',
             'email.unique'                    => '邮箱已经被使用'
         ];
