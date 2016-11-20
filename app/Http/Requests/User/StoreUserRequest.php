38a0e471a227b29bc4c88c999658a375e5f08ae8
wcgInTaoCheLe <milan015@sina.com>
2016-11-20 16:43:03 +0800

    用户添加验证规则

--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -18,22 +18,43 @@ class StoreUserRequest extends Request
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * 创建用户验证规则
      * @return array
      */
     public function rules()
-    {
+    {   
         return [
-            'name' => 'required',
-            'email' => 'required|email',
-            'address' => '',
-            'work_number' => 'numeric',
-            'personal_number' => 'numeric',
-            'password' => 'required|min:5|confirmed',
-            'password_confirmation' => 'required|min:5',
-            'image_path' => '',
-            'roles' => 'required',
-            'departments' => 'required'
+            'name'                  => 'required|unique:tcl_user',
+            'nick_name'             => 'required|unique:tcl_user',
+            'password'              => 'required|min:6|confirmed',
+            'password_confirmation' => 'required|min:6',
+            'telephone'             => 'required|phone_number',
+            // 'telephone'             => 'phone_number',
+            'email'                 => 'email|unique:tcl_user',
+            'role_id'               => 'required',
+            'shop_id'               => 'required'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 验证失败信息提示
+     * @return array
+     */
+    public function messages(){
+        return [
+            'name.required'                   => '请输入用户名',
+            'name.unique'                     => '用户名重复',
+            'nick_name.unique'                => '实际姓名重复',
+            'nick_name.required'              => '请输入实际姓名',
+            'password.required'               => '请输入密码',
+            'password.confirmed'              => '两次输入的密码不一致',
+            'password_confirmation.required'  => '请确认密码',
+            'telephone.required'              => '请输入手机号码',
+            'telephone.phone_number'          => '手机号码无效',
+            'role_id.required'                => '请选择角色',
+            'shop_id.required'                => '请选择门店',
+            'email.unique'                    => '邮箱已经被使用'
         ];
     }
 }
