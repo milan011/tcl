db44bc03dfabe857ed4ee00ea2919d1806923ce5
wcgInTaoCheLe <milan015@sina.com>
2016-11-23 16:42:02 +0800

    品牌管理模板修改

--- resources/views/admin/brand/create.blade.php
+++ resources/views/admin/brand/create.blade.php
@@ -10,10 +10,10 @@
 	</li>
 	<li>
 		<i class="icon-home"></i>
-		<a href="{{route('admin.shop.index')}}">门店管理</a> 
+		<a href="{{route('admin.brand.index')}}">品牌管理</a> 
 		<i class="icon-angle-right"></i>
 	</li>
-	<li><a href="#1f">添加门店</a></li>
+	<li><a href="#1f">添加品牌</a></li>
 </ul>
 @endsection
 <!-- 主体 -->
@@ -24,105 +24,59 @@
 <div class="row-fluid sortable">
 	<div class="box span12">
 		<div class="box-content">
-			<form class="form-horizontal" action="{{route('admin.shop.store')}}" method="post">
+			<form class="form-horizontal" action="{{route('admin.brand.store')}}" method="post">
 				{!! csrf_field() !!}
 				<fieldset>
 				  <div class="control-group">
-					<label class="control-label" for="focusedInput">门店名称</label>
+					<label class="control-label" for="focusedInput">品牌名称</label>
 					<div class="controls">
 					  <input class="input-xlarge focused" id="name" name="name" type="text" value="{{old('name')}}">
 					</div>
 				  </div>
+				  <div class="control-group">
+					<label class="control-label" for="focusedInput">品牌Logo</label>
+					<div class="controls">
+					  <input class="input-xlarge focused" id="logo_img" name="logo_img" type="file" value="{{old('logo_img')}}">
+					  <a  id="upload-button" href="#" onclick="saveImg()" class="btn btn-primary" style="margin-left:10px;">上传</a>
+					</div>					
+				  </div>
 				  <!-- <div class="control-group">
-				  					<label class="control-label">上级门店</label>
-				  					<div class="controls">
-				  					  <span class="input-xlarge uneditable-input">淘车乐总公司</span>
-				  					  <input type="hidden" id="PID" name="PID" value="1"/>
-				  					</div>
-				  </div> -->
-				    <!-- <div id="dept_view"  class="alert alert-error" style="display:none">
-				    						<button class="close" data-dismiss="alert" type="button">×</button>
-				    						<strong>上级门店不能为空!</strong> 请返回选择.
-				    					</div> -->
+					<a id="upload-button" href="#" onclick="saveImg()" class="btn btn-primary" style="margin-left:200px;">上传</a>
+					<img id="loading" src="/metro/img/ajax-loader.gif" style="margin-left:200px;display:none;">
+					</div> -->
+				  <div class="control-group">
+					<label class="control-label" for="focusedInput">品牌首字母</label>
+					<div class="controls">
+					  <input class="input-xlarge focused" id="frist_letter" name="frist_letter" type="text" value="{{old('frist_letter')}}">
+					</div>
+				  </div>
+				  <div class="control-group">
+					<label class="control-label" for="focusedInput">品牌排序</label>
+					<div class="controls">
+					  <input class="input-xlarge focused" id="sort" name="sort" type="text" value="{{old('sort')}}">
+					</div>
+				  </div>
+
 				<div class="control-group">
 					<label class="control-label" for="selectError3">是否启用</label>
 					<div class="controls">
-					  <select id="status" name="status" data-rel="chosen">
+					  <select id="status" name="status">
 					  	<option  value="1">启用</option>
 						<option  value="0">停用</option>
 						
 						</select>
 					</div>
-				  </div>
-				
-				<div class="control-group">
-					<label class="control-label" for="selectError3">门店类别</label>
-					<div class="controls">
-					  <select id="type" name="type" data-rel="chosen">
-					  	<option  value="1">门店</option>
-					  	<option  value="2">加盟店</option>
-						</select>
-					</div>
-				</div>	
-				<div class="control-group">
-					<label class="control-label" for="selectError3">所属省份</label>
-					<div class="controls">
-					  <select id="provence_id" name="provence_id" data-rel="chosen">
-					  	<option  value="1">河北</option>
-					  	<option  value="2">河南</option>
-						</select>
-					</div>					
-				</div>	
-				<div class="control-group">
-					<label class="control-label" for="selectError3">所属城市</label>
-					<div class="controls">
-					  <select id="city_id" name="city_id" data-rel="chosen">
-					  	<option  value="1">石家庄</option>
-					  	<option  value="2">郑州</option>
-						</select>
-					</div>					
-				</div>		  
-				<div class="control-group">
-					<label class="control-label" for="focusedInput">地址</label>
-					<div class="controls">
-						 <textarea id="address" name="address" class="autogrow"></textarea>
-					</div>
-				  </div>
-
-				   <!-- <div class="control-group">
-				   					<label class="control-label" for="focusedInput">手机号</label>
-				   					<div class="controls">
-				   					  <input class="input-xlarge focused" id="mobile" name="mobile" type="text" value="">
-				   					</div>
-				   				  </div> -->
+				  </div>	
 
 				  <div class="control-group">
-				  					<label class="control-label" for="focusedInput">联系电话</label>
-				  					<div class="controls">
-				  					  <input class="input-xlarge focused" id="telephone" name="telephone" type="text" value="{{old('telephone')}}">
-				  					</div>
-				  </div>
-
-				   <div class="control-group">
-					<label class="control-label" for="focusedInput">QQ号</label>
+					<label class="control-label" for="selectError3">是否推荐</label>
 					<div class="controls">
-					  <input class="input-xlarge focused" id="qq_number" name="qq_number" type="text" value="{{old('qq_number')}}">
+					  <select id="recommend" name="recommend" >
+					  	<option  value="1">推荐</option>
+						<option  value="0">不推荐</option>						
+						</select>
 					</div>
-				  </div>
-
-				   <div class="control-group">
-					<label class="control-label" for="focusedInput">微信公共号</label>
-					<div class="controls">
-					  <input class="input-xlarge focused" id="wx_number" name="wx_number" type="text" value="{{old('wx_number')}}">
-					</div>
-				  </div>
-
-				   <div class="control-group">
-					<label class="control-label" for="focusedInput">常用邮箱</label>
-					<div class="controls">
-					  <input class="input-xlarge focused" id="email" name="email" type="text" value="{{old('email')}}">
-					</div>
-				  </div>				
+				  </div>	  				
 				  <div class="form-actions">
 					<button type="submit"  onclick="check()" class="btn btn-primary">确定</button>
 					<button class="btn" onclick="window.history.go(-1);return false;">返回</button>
@@ -132,4 +86,11 @@
 		</div>
 	</div>			
 </div>   
+@endsection
+@section('script_content')
+<!-- 引入确认框js -->
+<script src="{{URL::asset('js/tcl/dynamic.js')}}"></script> 
+<script>
+
+</script>
 @endsection
--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -11,7 +11,7 @@
 			<a href="{{route('admin.index')}}">主页</a>  
 			<i class="icon-angle-right"></i>
 		</li>
-		<li><a href="#1f">门店管理</a></li>
+		<li><a href="#1f">品牌管理</a></li>
 	</ul>
 @endsection
 
@@ -20,20 +20,10 @@
 @include('layouts.message')
 	<div class="row-fluid sortable">		
 		<div class="box span12">
-			<!-- <div class="box-header" data-original-title>
-				<h2>
-					<i class="halflings-icon user"></i><span class="break"></span>Members
-				</h2>
-				<div class="box-icon">
-					<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
-					<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
-					<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
-				</div>
-			</div> -->
 			<div class="box-content">
 				<ul style="background: none repeat scroll 0 0 #eee;border: 0 none;border-radius: 0;box-shadow: none;color: #aaa;line-height: 34px; margin: 0;">
 					<li style="display: inline-block;line-height: 20px;">
-						<a class="btn btn-primary" href="{{route('admin.shop.create')}}">添加门店</a>
+						<a class="btn btn-primary" href="{{route('admin.brand.create')}}">添加品牌</a>
 					</li>
 					<li style="display: inline-block;line-height: 20px;">
 						<a href="#" onclick="window.history.go(-1);return false;" class="btn ">返回</a>
@@ -42,31 +32,27 @@
 				<table class="table table-striped table-bordered">
 					<thead>
 						<tr>
-							<th>门店名称</th>
-							<th>门店类别</th>
-							<th>门店状态</th>
+							<th>品牌名称</th>
+							<th>品牌Logo</th>
+							<th>品牌状态</th>
 							<th>操作</th>
 						</tr>
 					</thead>   
 					<tbody>
-						@foreach ($shops as $shop)
+						@foreach ($brands as $brand)
     					<tr>
-							<td>{{$shop->name}}</td>
-							@if($shop->type == '1')
-							<td class="center">门店</td>
-							@else
-							<td class="center">加盟店</td>
-							@endif
-							@if($shop->status == '1')
+							<td>{{$brand->name}}</td>
+							<td>{{$brand->logo_img}}</td>							
+							@if($brand->status == '1')
 							<td class="center"><span class="label label-success">已启用</span></td>
 							@else
 							<td class="center"><span class="label label-warning">已停用</span></td>
 							@endif							
 							<td class="center">
-								<a class="btn btn-warning" href="{{route('admin.shop.edit', ['shop'=>$shop->id])}}">
+								<a class="btn btn-warning" href="{{route('admin.brand.edit', ['brand'=>$brand->id])}}">
 									<i class="icon-edit icon-white"></i> 编辑
 								</a>
-								@if($shop->status == '1') 
+								@if($brand->status == '1') 
 								<button class="btn btn-info changStatus" data-status="0">
 									<i class="icon-edit icon-white"></i> 停用
 								</button>
@@ -75,9 +61,9 @@
 									<i class="icon-edit icon-white"></i> 启用
 								</button>
 								@endif
-								<input type="hidden" value="{{$shop->id}}">
+								<input type="hidden" value="{{$brand->id}}">
 								<span>
-								<form action="{{route('admin.shop.destroy', ['shop'=>$shop->id])}}" method="post" style="display: inherit;margin:0px;">
+								<form action="{{route('admin.brand.destroy', ['brand'=>$brand->id])}}" method="post" style="display: inherit;margin:0px;">
 									{{ csrf_field() }}
             						{{ method_field('DELETE') }}
 									<button class="btn btn-danger delete-confrim" type="button">
@@ -91,7 +77,7 @@
 					</tbody>
 				</table>
 				<div class="pagination pagination-centered">
-					 {!! $shops->links() !!}
+					 {!! $brands->links() !!}
 				</div>          
 			</div>
 		</div>
@@ -103,47 +89,5 @@
 <script src="{{URL::asset('js/tcl/confirm.js')}}"></script> 
 <script>
 
-	$(document).ready(function(){
-
-		$('.changStatus').click(function(){
-
-			var id     = $(this).next().val();
-			var status = $(this).attr('data-status');
-			var token = $("input[name='_token']").val();
-
-			/*alert(id);
-			alert(status);*/
-			// alert($("input[name='_token']").val());
-
-			$.ajax({
-
-				type: 'POST',
-
-				url: 'shop/changeStatus',
-
-				data: { id : id, status : status},
-
-				dataType: 'json',
-
-				headers: {
-
-					'X-CSRF-TOKEN': '{{ csrf_token() }}'
-
-				},
-
-				success: function(data){
-
-					alert(data.msg);
-					location.reload();
-					// console.log(data);
-				},
-
-				error: function(xhr, type){
-
-					alert('Ajax error!')
-				}
-			});
-		});
-	});
 </script>
 @endsection
