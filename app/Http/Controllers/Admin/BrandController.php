10ae83549c8dd20bc0b6f8c6bb10b6fd3b678e30
wcgInTaoCheLe <milan015@sina.com>
2016-11-23 16:40:04 +0800

    淘车乐品牌功能控制器

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -6,32 +6,53 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Brand\UpdateBrandRequest;
+use App\Http\Requests\Brand\StoreBrandRequest;
+use App\Repositories\Brand\BrandRepositoryContract;
 
 class BrandController extends Controller
 {
+    protected $brand;
+
+    public function __construct(
+
+        BrandRepositoryContract $brands
+    ) {
+    
+        $this->brands = $brands;
+
+        // $this->middleware('brand.create', ['only' => ['create']]);
+    }
+
     /**
      * Display a listing of the resource.
-     *
+     * 品牌列表
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $brands = $this->brands->getAllBrands();
+        // dd(lastSql());
+        // dd($brands);
+        /*foreach ($Brands as $key => $value) {
+           dd($value->belongsToShop);
+        }*/
+        return view('admin.brand.index', compact('brands'));
     }
 
     /**
      * Show the form for creating a new resource.
-     *
+     * 创建品牌
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('admin.brand.create');
     }
 
     /**
      * Store a newly created resource in storage.
-     *
+     * 存储品牌
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -53,7 +74,7 @@ class BrandController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
+     * 编辑品牌
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
@@ -64,7 +85,7 @@ class BrandController extends Controller
 
     /**
      * Update the specified resource in storage.
-     *
+     * 更新品牌
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
