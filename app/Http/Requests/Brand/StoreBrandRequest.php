d3aac582fed0629324a94a0619331c0d70d8b9f1
wcgInTaoCheLe <milan015@sina.com>
2016-11-25 17:11:23 +0800

    图片处理验证规则

--- app/Http/Requests/Brand/StoreBrandRequest.php
+++ app/Http/Requests/Brand/StoreBrandRequest.php
@@ -13,18 +13,41 @@ class StoreBrandRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * 验证规则
      * @return array
      */
     public function rules()
     {
+        // dd($this->all());
         return [
-            //
+            'name'         => 'required|unique:tcl_brand',
+            'logo_img'     => 'required|image|unique:tcl_brand',
+            'frist_letter' => 'required|alpha|min:1|max:1',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 验证失败信息提示
+     * @return array
+     */
+    public function messages(){
+        return [
+            'name.required'         => '请输入品牌名',
+            'name.unique'           => '品牌名重复',
+            'logo_img.unique'       => '车型图片名称重复',
+            'logo_img.required'     => '请添加车型图片',
+            'logo_img.image'        => '添加文件必须是图片',
+            'frist_letter.required' => '请输车型首字母',
+            'frist_letter.alpha'    => '车型首字母必须是字母',
+            'frist_letter.min'      => '请输入一位字母',
+            'frist_letter.max'      => '请输入一位字母',
+            
         ];
     }
 }
