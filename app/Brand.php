45c1d0d335ed267e26b8ed5e227419342ea4ec02
wcgInTaoCheLe <milan015@sina.com>
2016-11-29 16:53:41 +0800

    车型、品牌model进展

--- app/Brand.php
+++ app/Brand.php
@@ -45,4 +45,10 @@ class Brand extends Model
 
       return $this->belongsTo('App\User', 'creater_id', 'id');
     }
+
+    // 定义Brand表与Category表一对多关系
+    public function hasManyCategory(){
+
+      return $this->hasMany('App\Category', 'brand_id', 'id');
+    }
 }
--- app/Category.php
+++ app/Category.php
@@ -3,16 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     /**
      * The database table used by the model.
      * 定义模型对应数据表及主键
      * @var string
      */
     // protected $table = 'users';
-    protected $table = 'tcl_shop';
+    protected $table = 'tcl_category';
     protected $primaryKey ='id';
 
     /**
@@ -20,7 +23,7 @@ class Category extends Model
      * 定义可批量赋值字段
      * @var array
      */
-    protected $fillable = ['name', 'nick_name', 'password', 'telephone', 'phone', 'qq_number', 'wx_number', 'address', 'creater_id', 'shop_id', 'status', 'user_img'];
+    protected $fillable = ['name', 'logo_img', 'sort', 'status', 'recommend', 'user_id', 'brand_id'];
 
     /**
      * The attributes excluded from the model's JSON form.
@@ -30,4 +33,17 @@ class Category extends Model
     protected $hidden = [   
         'password', 'remember_token',
     ];
+
+    /**
+     * 应该被调整为日期的属性
+     * 定义软删除
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    // 定义Brand表与Category表一对多关系
+    public function belongsToBrand(){
+
+      return $this->belongsTo('App\Brand', 'brand_id', 'id');
+    }
 }
