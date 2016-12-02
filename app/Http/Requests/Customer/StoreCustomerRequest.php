286a72838b74928ccd2841f86fc85d625644a110
wcgInTaoCheLe <milan015@sina.com>
2016-12-02 16:50:55 +0800

    淘车乐客户管理验证规则添加

--- app/Http/Requests/Customer/StoreCustomerRequest.php
+++ app/Http/Requests/Customer/StoreCustomerRequest.php
@@ -13,18 +13,34 @@ class StoreCustomerRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * 验证规则
      * @return array
      */
     public function rules()
     {
+        // dd($this->all());
         return [
-            //
+            // 'name'     => 'required|unique:tcl_category',
+            'name'      => 'required',
+            'telephone' => 'required|phone_number',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 验证失败信息提示
+     * @return array
+     */
+    public function messages(){
+        return [
+            'name.required'          => '请输入客户名称',
+            'telephone.required'     => '请输入手机号码',
+            'telephone.phone_number' => '手机号码无效',
         ];
     }
 }
--- app/Http/Requests/Customer/UpdateCustomerRequest.php
+++ app/Http/Requests/Customer/UpdateCustomerRequest.php
@@ -18,13 +18,29 @@ class UpdateCustomerRequest extends Request
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * 更新用户验证规则
      * @return array
      */
     public function rules()
     {
+        $id = $this->route('user');
+        
         return [
-            //
+            'name'      => 'required',
+            'telephone' => 'required|phone_number',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 验证失败信息提示
+     * @return array
+     */
+    public function messages(){
+        return [
+            'name.required'          => '请输入客户名称',
+            'telephone.required'     => '请输入手机号码',
+            'telephone.phone_number' => '手机号码无效',
         ];
     }
 }
