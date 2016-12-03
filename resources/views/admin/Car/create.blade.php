c5bf95d1747c884bbd0ee555d29bfb7515e612c4
wcgInTaoCheLe <milan015@sina.com>
2016-12-03 17:03:22 +0800

    客户管理模板改进

--- resources/views/admin/Car/create.blade.php
+++ resources/views/admin/Car/create.blade.php
@@ -10,10 +10,10 @@
 	</li>
 	<li>
 		<i class="icon-home"></i>
-		<a href="{{route('admin.category.index')}}">车型列表</a> 
+		<a href="{{route('car.self')}}">我的车源</a> 
 		<i class="icon-angle-right"></i>
 	</li>
-	<li><a href="#1f">添加车型</a></li>
+	<li><a href="#1f">添加车源</a></li>
 </ul>
 @endsection
 <!-- 主体 -->
@@ -22,21 +22,61 @@
 @include('layouts.message')
 
 <div class="row-fluid sortable" style="height:100%;">
+
 	<div class="box span12" style="height:100%;">
-		<div class="box-content" style="overflow: auto;height: 80%;">
-			<form class="form-horizontal" action="{{route('admin.category.store')}}" method="post" enctype="multipart/form-data">
+		<div class="box-header">
+			<h2><i class="halflings-icon  icon-truck"></i><span class="break"></span><span id="content_title">客户信息</span></h2>
+		<div class="box-icon">
+			<a href="#" class="btn-minimize"><i class="halflings-icon  chevron-up"></i></a>
+		</div>
+	</div>
+	<div class="box-content" id="customer_content">
+		<form id="customer_form" class="form-horizontal" action="" method="post">
+			{!! csrf_field() !!}
+			<fieldset>
+				<div class="control-group">
+					<label class="control-label" for="customer_name">客户姓名</label>
+					<div class="controls">
+					  <input class="input-xlarge focused" id="customer_name" name="customer_name" type="text" value="">
+					</div>
+				 </div>
+				 <div class="control-group">
+					<label class="control-label" for="telephone">手机号</label>
+					<div class="controls">
+					  	<input class="input-xlarge focused" id="telephone" name="telephone" type="text" value="">
+					</div>
+				 </div>
+				<div class="control-group  ">
+                	<label class="control-label" for="customer_res">客户来源</label>
+                	<div class="controls">
+                  		<select id="customer_res" name="customer_res" >                        
+					  		@foreach($customer_res as $key=>$res)											
+					  			<option  value="{{$key}}">{{$res}}</option>	
+					  		@endforeach	                     
+                  		</select>
+                	</div>
+              	</div> 
+				<div class="form-actions">
+					<button class="btn" onclick="window.history.go(-1);return false;">返回</button>
+					<button id="customer_add" class="btn btn-primary">下一步</button>					
+				 </div>
+			</fieldset>
+		</form>
+	</div>
+		<div class="box-content" id="car_content" style="overflow: auto;height: 70%;display:none;">
+			<form class="form-horizontal" id="car_form" action="{{route('admin.category.store')}}" method="post" enctype="multipart/form-data">
 				{!! csrf_field() !!}
 				<fieldset>
 					<div class="control-group">
-					<label class="control-label" for="focusedInput">车型类别</label>
-					<div class="controls">
-					   <select id="category_type" name="category_type">
-					  		@foreach($category_type as $key=>$category)											
-					  		<option  value="{{$key}}">{{$category}}</option>	
-					  		@endforeach											
-						</select>
-					</div>
-				  </div>
+						<label class="control-label" for="focusedInput">车型类别</label>
+						<div class="controls">
+						   <select id="category_type" name="category_type">
+						  		@foreach($category_type as $key=>$category)											
+						  		<option  value="{{$key}}">{{$category}}</option>	
+						  		@endforeach											
+							</select>
+						</div>
+				  	</div>
 				  <div class="control-group">
 					<label class="control-label" for="selectError3">车型品牌</label>
 					<div class="controls">
@@ -102,19 +142,59 @@
                   		</select>
                 	</div>
               	</div>
+              	<div class="control-group">
+					<label class="control-label" for="plate_date">上牌日期</label>
+						<div class="controls">
+							<input type="text" class="input-xlarge datepicker" id="plate_date" value="">
+						</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="plate_end">到检日期</label>
+						<div class="controls">
+							<input type="text" class="input-xlarge datepicker" id="plate_end" value="">
+						</div>
+				</div>
+
+				<div class="control-group  ">
+                	<label class="control-label" for="shiftType">过户次数</label>
+                	<div class="controls">
+                  		<select id="sale_number" name="sale_number" >                        
+					  		@foreach($sale_number as $key=>$sale)											
+					  		<option  value="{{$key}}">{{$sale}}</option>	
+					  		@endforeach	                     
+                  		</select>
+                	</div>
+              	</div>
+              	<div class="control-group">
+					<label class="control-label" for="focusedInput">行驶里程</label>
+					<div class="controls">
+					  <input class="input-xlarge focused" id="mileage" name="mileage" type="text" value="{{old('mileage')}}"><span style="margin-left:5px;">万公里</span>
+					</div>
+				</div>
+
+				<div class="control-group ">
+					<label class="control-label" for="description">
+						<font style="color:red;">*&nbsp;</font>车况
+					</label>
+				<div class="controls">
+					<textarea id="description" name="description" required ></textarea>
+				</div>
+			  </div>
+
+			  <div class="control-group">
+					<label class="control-label" for="top_price">期望价格</label>
+					<div class="controls">
+					  <input class="input-xlarge focused" id="top_price" name="top_price" type="text" value="{{old('top_price')}}"><span style="margin-left:5px;">万元</span>
+					</div>
+				</div>
 
 				<div class="control-group">
-					<label class="control-label" for="selectError3">是否启用</label>
+					<label class="control-label" for="bottom_price">底价</label>
 					<div class="controls">
-					  <select id="status" name="status">
-					  	<option  value="1">启用</option>
-						<option  value="0">停用</option>
-						
-						</select>
+					  <input class="input-xlarge focused" id="bottom_price" name="bottom_price" type="text" value="{{old('bottom_price')}}"><span style="margin-left:5px;">万元</span>
 					</div>
-				  </div>	
-
-				  <div class="control-group">
+				</div>
+				<div class="control-group">
 					<label class="control-label" for="selectError3">是否推荐</label>
 					<div class="controls">
 					  <select id="recommend" name="recommend" >
@@ -122,11 +202,45 @@
 						<option  value="0">不推荐</option>						
 						</select>
 					</div>
-				  </div>	  				
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="is_top">是否置顶</label>
+					<div class="controls">
+					  <select id="is_top" name="is_top" >
+					  	<option  value="1">置顶</option>
+						<option  value="0">不置顶</option>						
+						</select>
+					</div>
+				</div>
+				<div class="control-group  ">
+                	<label class="control-label" for="car_type">过户次数</label>
+                	<div class="controls">
+                  		<select id="car_type" name="car_type" >                        
+					  		@foreach($car_type as $key=>$type)											
+					  		<option  value="{{$key}}">{{$type}}</option>	
+					  		@endforeach	                     
+                  		</select>
+                	</div>
+              	</div>
+
+
+				<div class="control-group">
+					<label class="control-label" for="selectError3">是否启用</label>
+					<div class="controls">
+					  <select id="status" name="status">
+					  	<option  value="1">启用</option>
+						<option  value="0">停用</option>						
+						</select>
+					</div>
+				  </div>	
+	  				
 				  <div class="form-actions">
 				  	<input type="hidden" name="ajax_request_url" value="{{route('brand.getChildBrand')}}">
-					<button type="submit" class="btn btn-primary">确定</button>
-					<button class="btn" onclick="window.history.go(-1);return false;">返回</button>
+				  	<input type="hidden" name="shop_id" value="{{Auth::user()->shop_id}}">
+				  	<input type="hidden" name="want_area" value="{{$city_id}}">					
+				  	<input type="hidden" name="customer_id" value="">					
+					<button class="btn" id="return_customer">返回</button>
+					<button id="car_add" type="submit" class="btn btn-primary">确定</button>
 				  </div>
 				</fieldset>
 			</form>				
@@ -137,4 +251,59 @@
 @section('script_content')
 <!-- 引入车型级联js -->
 <script src="{{URL::asset('js/tcl/category.js')}}"></script> 
+<script>
+	$(document).ready(function(){
+
+		var request_url      = '{{route('admin.customer.ajaxStore')}}';
+		/*var customer_name    = $('#customer_name').val();
+		var telephone        = $('#telephone').val();
+		var customer_res     = $('#customer_res').val();*/
+		var customer_content = $('#customer_content');
+		var car_content      = $('#car_content');
+		var customer_id      = $("input[name='customer_id']");		
+
+		$('#customer_add').click(function(){
+
+			$.ajax({
+				method: 'POST',
+				url: request_url,
+				data:$("#customer_form").serialize(),
+				dataType: 'json',
+				headers: {		
+					'X-CSRF-TOKEN': '{{ csrf_token() }}'		
+				},
+				success:function(data){
+
+					//设置新加车源对应的客户ID
+					customer_id.val(data.id);
+					customer_content.hide();
+					car_content.show();
+					$('#content_title').text('车源信息');
+					// alert($("input[name='customer_id']").val());
+				},
+				error: function(xhr, type){
+					/*console.log(JSON.parse(xhr.responseText));
+					console.log(JSON.parse(xhr.responseText).customer_name[0]);
+					console.log(JSON.parse(xhr.responseText).telephone[0]);
+					console.log(xhr.responseText);*/
+
+					if(xhr.status == 422){ //表单验证失败，返回的状态
+
+						alert('请输入用户名和真实手机号码');
+
+						return false;
+					}
+					alert('添加用户失败，请重新添加或联系管理员');
+				}
+			});
+			return false;
+		});
+
+		$('#return_customer').click(function(){
+
+			customer_content.show();
+			car_content.hide();
+		});
+	});
+</script>
 @endsection
