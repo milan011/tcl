d27ecafd185746f07633c01e3d95187e1cb09573
wcgInTaoCheLe <milan015@sina.com>
2016-12-04 16:30:57 +0800

    车源验证规则添加

--- app/Http/Requests/Cars/StoreCarsRequest.php
+++ app/Http/Requests/Cars/StoreCarsRequest.php
@@ -13,18 +13,47 @@ class StoreCarsRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * 验证规则
      * @return array
      */
     public function rules()
     {
+
         return [
-            //
+            'name'           => 'required',
+            'capacity'       => 'required',
+            'plate_date'     => 'required|date',
+            'plate_end'      => 'required|date',
+            'mileage'        => 'required',
+            'description'    => 'required',
+            'top_price'      => 'required',
+            'bottom_price'   => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 验证失败信息提示
+     * @return array
+     */
+    public function messages(){
+        return [
+            'name.required'         => '请确认车型名称',
+            'capacity.required'     => '请输入排量',
+            'plate_date.required'   => '请输入上牌日期',
+            'plate_date.date'       => '日期格式错误',
+            'plate_end.required'    => '请输到检日期',
+            'plate_end.date'        => '日期格式错误',
+            'mileage.required'      => '请输入行驶里程',
+            'description.required'  => '请输入车况描述',
+            'top_price.required'    => '请输入期望价格',
+            'bottom_price.required' => '请输入底价',
+
         ];
     }
 }
