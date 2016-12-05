93d409559b7bdca8dd358156357701c0f9f34bb8
wcgInTaoCheLe <milan015@sina.com>
2016-12-05 17:18:54 +0800

    确认框改进

--- public/js/tcl/confirm.js
+++ public/js/tcl/confirm.js
@@ -2,17 +2,17 @@
 $('.delete-confrim').click(function(){
 	var obj = $(this);
 	$.confirm({
-       	title: '注意!',
-       	content: '确实要删除吗?',
-       	cancelButton: '取消',
-       	confirmButtonClass: 'btn-danger',
-       	confirm: function () {
-       	    obj.parent('form').submit();
-       	    // console.log(obj.parent('form'));
-       	    // return false;
-       	},
-       	cancel: function () {
-       	    return false;
-       	}
+        title: '注意!',
+        content: '确实要删除吗?',
+        cancelButton: '取消',
+        confirmButtonClass: 'btn-danger',
+        confirm: function () {
+            obj.parent('form').submit();
+            // console.log(obj.parent('form'));
+            // return false;
+        },
+        cancel: function () {
+            return false;
+        }
     });
 });
