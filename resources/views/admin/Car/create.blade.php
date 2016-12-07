d43f637c80ec22f120da64903840886ff37e0c9f
wcgInTaoCheLe <milan015@sina.com>
2016-12-07 16:49:12 +0800

    车源图片添加模板改进

--- resources/views/admin/Car/create.blade.php
+++ resources/views/admin/Car/create.blade.php
@@ -3,6 +3,7 @@
 @section('head_content')
 <!-- 自定义css -->
 <link id="bootstrap-style" href="{{ URL::asset('css/tcl/bootstrap-datepicker.min.css') }}" rel="stylesheet">
+<link id="bootstrap-style" href="{{ URL::asset('css/tcl/dropzone/dropzone.css') }}" rel="stylesheet">
 @endsection
 
 <!-- 面包屑 -->
@@ -27,7 +28,7 @@
 @include('layouts.message')
 
 <div class="row-fluid sortable" style="height:100%;">
-
+	<!-- 客户信息添加 begin -->
 	<div class="box span12" style="height:100%;">
 		<div class="box-header">
 			<h2><i class="halflings-icon  icon-truck"></i><span class="break"></span><span id="content_title">客户信息</span></h2>
@@ -35,6 +36,8 @@
 			<a href="#" class="btn-minimize"><i class="halflings-icon  chevron-up"></i></a>
 		</div>
 	</div>
+	<!-- 客户信息添加 end -->
+	<!-- 车源信息添加 begin-->
 	<div class="box-content" id="customer_content">
 		<form id="customer_form" class="form-horizontal" action="" method="post">
 			{!! csrf_field() !!}
@@ -296,6 +299,80 @@
 				</fieldset>
 			</form>				
 		</div>
+		<!-- 车源信息添加 end-->
+		<!-- 图片信息添加 begin -->
+		<div class="box-content" id="img_content" style="overflow: auto;height: 70%;display:none;">
+			<h3>Images <span id="photoCounter"></span></h3><br />
+			<!-- 隐藏图片信息模板 -->
+    		<div id="preview-template" style="display: none;">
+
+        		<div class="dz-preview dz-file-preview">
+            		<div class="dz-image"><img data-dz-thumbnail="">
+            	</div>
+
+            	<div class="dz-details">
+            	    <div class="dz-size"><span data-dz-size=""></span></div>
+            	    <div class="dz-filename"><span data-dz-name=""></span></div>
+            	</div>
+            	<div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress=""></span></div>
+            	<div class="dz-error-message"><span data-dz-errormessage=""></span></div>
+
+            	<div class="dz-success-mark">
+                	<svg width="54px" height="54px" viewBox="0 0 54 54" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                    <!-- Generator: Sketch 3.2.1 (9971) - http://www.bohemiancoding.com/sketch -->
+                    	<title>Check</title>
+                    	<desc>Created with Sketch.</desc>
+                    	<defs></defs>
+                    	<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                        <path d="M23.5,31.8431458 L17.5852419,25.9283877 C16.0248253,24.3679711 13.4910294,24.366835 11.9289322,25.9289322 C10.3700136,27.4878508 10.3665912,30.0234455 11.9283877,31.5852419 L20.4147581,40.0716123 C20.5133999,40.1702541 20.6159315,40.2626649 20.7218615,40.3488435 C22.2835669,41.8725651 24.794234,41.8626202 26.3461564,40.3106978 L43.3106978,23.3461564 C44.8771021,21.7797521 44.8758057,19.2483887 43.3137085,17.6862915 C41.7547899,16.1273729 39.2176035,16.1255422 37.6538436,17.6893022 L23.5,31.8431458 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z" id="Oval-2" stroke-opacity="0.198794158" stroke="#747474" fill-opacity="0.816519475" fill="#FFFFFF" sketch:type="MSShapeGroup"></path>
+                    	</g>
+                	</svg>
+            	</div>
+
+            	<div class="dz-error-mark">
+                	<svg width="54px" height="54px" viewBox="0 0 54 54" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                    <!-- Generator: Sketch 3.2.1 (9971) - http://www.bohemiancoding.com/sketch -->
+                    	<title>error</title>
+                    	<desc>Created with Sketch.</desc>
+                    	<defs></defs>
+                    	<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                        		<g id="Check-+-Oval-2" sketch:type="MSLayerGroup" stroke="#747474" stroke-opacity="0.198794158" fill="#FFFFFF" fill-opacity="0.816519475">
+                            	<path d="M32.6568542,29 L38.3106978,23.3461564 C39.8771021,21.7797521 39.8758057,19.2483887 38.3137085,17.6862915 C36.7547899,16.1273729 34.2176035,16.1255422 32.6538436,17.6893022 L27,23.3431458 L21.3461564,17.6893022 C19.7823965,16.1255422 17.2452101,16.1273729 15.6862915,17.6862915 C14.1241943,19.2483887 14.1228979,21.7797521 15.6893022,23.3461564 L21.3431458,29 L15.6893022,34.6538436 C14.1228979,36.2202479 14.1241943,38.7516113 15.6862915,40.3137085 C17.2452101,41.8726271 19.7823965,41.8744578 21.3461564,40.3106978 L27,34.6568542 L32.6538436,40.3106978 C34.2176035,41.8744578 36.7547899,41.8726271 38.3137085,40.3137085 C39.8758057,38.7516113 39.8771021,36.2202479 38.3106978,34.6538436 L32.6568542,29 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z" id="Oval-2" sketch:type="MSShapeGroup"></path>
+                        		</g>
+                    		</g>
+                		</svg>
+            		</div>
+        		</div>
+    		</div>
+    		<!-- End 隐藏图片信息模板 -->
+			<form id="real-dropzone" class="form-horizontal dropzone" action="{{route('admin.image.upload')}}" method="post" enctype="multipart/form-data">
+			{!! csrf_field() !!}
+				<div class="dz-message">
+	
+            	</div>
+            	<div class="fallback">
+                    <input name="file" type="file" multiple />
+                </div>
+                <div class="dropzone-previews" id="dropzonePreview"></div>
+
+                <h4 style="text-align: center;color:#428bca;">Drop images in this area  <span class="glyphicon glyphicon-hand-down"></span></h4>
+
+                <div class="form-actions">				
+				  	<input type="hidden" name="car_id" value="">					
+					<button class="btn" id="return_car">返回</button>
+					<button id="img_add" class="btn btn-primary">确定</button>
+				 </div>
+			</form>
+			<div class="jumbotron how-to-create">
+                <ul>
+                    <li>Images are uploaded as soon as you drop them</li>
+                    <li>Maximum allowed size of image is 8MB</li>
+                </ul>
+
+            </div>
+            
+		</div>
+		<!-- 图片信息添加 end -->
 	</div>			
 </div>   
 @endsection
@@ -307,6 +384,8 @@
 <!-- 引入日历插件 -->
 <script src="{{URL::asset('js/tcl/bootstrap-datepicker.js')}}"></script> 
 <script src="{{URL::asset('js/tcl/locales/bootstrap-datepicker.zh-CN.js')}}"></script> 
+<script src="{{URL::asset('js/tcl/dropzone/dropzone.js')}}"></script> 
+<script src="{{URL::asset('js/tcl/dropzone-config.js')}}"></script> 
 <script>
 	$(document).ready(function(){
 
@@ -316,7 +395,9 @@
 		var customer_res     = $('#customer_res').val();*/
 		var customer_content = $('#customer_content');
 		var car_content      = $('#car_content');
+		var img_content      = $('#img_content');
 		var customer_id      = $("input[name='customer_id']");	
+		var car_id           = $("input[name='car_id']");	
 
 		$('.date-picker').datepicker({
             language: 'zh-CN',
@@ -376,8 +457,11 @@
 				},
 				success:function(data){
 
-					console.log(data);
-					alert('车源添加成功！');
+					//设置图片对应车源ID
+					car_id.val(data.id);
+					$('#content_title').text('图片上传');
+					car_content.hide();
+					img_content.show();
 				},
 				error: function(xhr, type){
 					
