86d6cb267ee513de91d99521e90c15bf269e6730
wcgInTaoCheLe <milan015@sina.com>
2016-12-07 16:47:16 +0800

    图片上传插件dropzone css及js添加

---
+++ public/js/tcl/dropzone/dropzone-config.js
@@ -0,0 +1,53 @@
+var photo_counter = 0;
+Dropzone.options.realDropzone = {
+
+    uploadMultiple: false,
+    parallelUploads: 10,
+    maxFilesize: 2,
+    previewsContainer: '#dropzonePreview',
+    previewTemplate: document.querySelector('#preview-template').innerHTML,
+    addRemoveLinks: true,
+    dictRemoveFile: 'Remove',
+    dictFileTooBig: '图片不能大于MB',
+
+    // The setting up of the dropzone
+    init:function() {
+
+        this.on("removedfile", function(file) {
+
+            $.ajax({
+                type: 'POST',
+                url: 'imgUpload/delete',
+                data: {id: file.name},
+                dataType: 'html',
+                success: function(data){
+                    var rep = JSON.parse(data);
+                    if(rep.code == 200)
+                    {
+                        photo_counter--;
+                        $("#photoCounter").text( "(" + photo_counter + ")");
+                    }
+
+                }
+            });
+
+        } );
+    },
+    error: function(file, response) {
+        if($.type(response) === "string")
+            var message = response; //dropzone sends it's own error messages in string
+        else
+            var message = response.message;
+        file.previewElement.classList.add("dz-error");
+        _ref = file.previewElement.querySelectorAll("[data-dz-errormessage]");
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            node = _ref[_i];
+            _results.push(node.textContent = message);
+        }
+        return _results;
+    },
+    success: function(file,done) {
+        photo_counter++;
+        $("#photoCounter").text( "(" + photo_counter + ")");
+    }}
