30d36e2444bc81ef33d1bdb9d5323322061707cb
wcgInTaoCheLe <milan015@sina.com>
2016-12-08 16:51:33 +0800

    图片上传配置

---
+++ config/images.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$base_dir = public_path('uploads\car\\');
+	$date = date('Ym', time());
+
+	$full_size = $base_dir.$date.'\images\\';
+	$icon_size = $base_dir.$date.'\icon\\';
+
+	return [
+	    /*'full_size'   => env('UPLOAD_FULL_SIZE'),
+	    'icon_size'   => env('UPLOAD_ICON_SIZE'),*/
+	    'full_size'   => $full_size,
+	    'icon_size'   => $icon_size,
+
+	];
