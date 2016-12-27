df1a6142acb533dbd22b78788323647bdd88f9fb
wcgInTaoCheLe <milan015@sina.com>
2016-12-27 17:02:51 +0800

    交易验证规则添加

--- app/Http/Requests/Transcation/StoreTranscationRequest.php
+++ app/Http/Requests/Transcation/StoreTranscationRequest.php
@@ -13,18 +13,40 @@ class StoreTranscationRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * 验证规则
      * @return array
      */
     public function rules()
-    {
+    {        
         return [
-            //
+            'deal_price'        => 'required|numeric',
+            'earnest'           => 'numeric',
+            'first_pay'         => 'numeric',
+            'last_pay'          => 'numeric',
+            'commission'        => 'numeric',
+            'commission_infact' => 'numeric',          
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 验证失败信息提示
+     * @return array
+     */
+    public function messages(){
+        return [
+            'deal_price.numeric'        => '成交价应该是数字',
+            'deal_price.required'       => '请输入成交价格',
+            'earnest.numeric'           => '定金应该是数字',
+            'first_pay.numeric'         => '首款应该是数字',
+            'last_pay.numeric'          => '尾款应该是数字',
+            'commission.numeric'        => '应收佣金应该是数字',
+            'commission_infact.numeric' => '实收佣金应该是数字',
         ];
     }
 }
