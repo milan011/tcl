de72405fd8158a40cacb1036950300e57fae8a1a
wcgInTaoCheLe <milan015@sina.com>
2017-01-06 19:09:30 +0800

    车源model改进

--- app/Cars.php
+++ app/Cars.php
@@ -67,18 +67,84 @@ class Cars extends Model
             }           
         }
 
+        if(!empty($requestData['car_code'])){
+
+            $query = $query->where('car_code', $requestData['car_code']);
+
+            return $query;
+        }
+
         if(isset($requestData['car_status']) && $requestData['car_status'] != ''){
 
             $query = $query->where('car_status', $requestData['car_status']);
         }else{
 
             $query = $query->whereIn('car_status', ['1', '2', '3', '4', '5', '6']);
+        }  
+
+        if(!empty($requestData['gearbox'])){
+
+            $query = $query->where('gearbox', $requestData['gearbox']);
         }
 
-        if(!empty($requestData['car_code'])){
+        if(!empty($requestData['sale_number'])){
 
-            $query = $query->where('car_code', $requestData['car_code']);
+            $query = $query->where('sale_number', $requestData['sale_number']);
         }
+
+        if(!empty($requestData['out_color'])){
+
+            $query = $query->where('out_color', $requestData['out_color']);
+        }
+
+        if(!empty($requestData['capacity'])){
+
+            $query = $query->where('capacity', $requestData['capacity']);
+        }
+
+        if(!empty($requestData['category_type'])){
+
+            $query = $query->where('categorey_type', $requestData['category_type']);
+        } 
+
+        if(!empty($requestData['category_id'])){
+
+            $query = $query->where('category_id', $requestData['category_id']);
+        }else{
+
+            if(!empty($requestData['car_factory'])){
+               $query = $query->where('car_factory', $requestData['car_factory']); 
+            }else{
+
+                if(!empty($requestData['brand_id'])){
+                    $query = $query->where('brand_id', $requestData['brand_id']);
+                }
+           }
+        } 
+
+        if(!empty($requestData['begin_milage'])){
+            $query = $query->where('milage', '<=', $requestData['begin_milage']);
+        }
+        
+        if(!empty($requestData['end_milage'])){
+            $query = $query->where('milage', '>=', $requestData['end_milage']);
+        }
+
+        if(!empty($requestData['top_price'])){
+            $query = $query->where('top_price', '<=', $requestData['top_price']);
+        }
+        
+        if(!empty($requestData['bottom_price'])){
+            $query = $query->where('bottom_price', '>=', $requestData['bottom_price']);
+        }
+
+        if(!empty($requestData['end_date'])){
+            $query = $query->where('created_at', '<=', $requestData['end_date']);
+        }
+        
+        if(!empty($requestData['begin_date'])){
+            $query = $query->where('created_at', '>=', $requestData['begin_date']);
+        }    
 
         return $query;
     }
