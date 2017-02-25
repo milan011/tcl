7cda7110450db77e9fe5d95b7f171378c3d13922
wcgInTaoCheLe <milan015@sina.com>
2017-02-25 10:16:44 +0800

    车源添加城市验证规则

--- app/Http/Requests/Cars/UpdateCarsRequest.php
+++ app/Http/Requests/Cars/UpdateCarsRequest.php
@@ -31,6 +31,8 @@ class UpdateCarsRequest extends Request
             'plate_end'      => 'required|date',
             'mileage'        => 'required',
             'description'    => 'required',
+            'plate_provence' => 'required|numeric|min:1',
+            'plate_city'     => 'required|numeric|min:1',
             'top_price'      => 'required',
             'bottom_price'   => 'required',
             'bottom_price'   => 'required',
@@ -57,6 +59,8 @@ class UpdateCarsRequest extends Request
             'bottom_price.required' => '请输入底价',
             'vin_code.alpha_num'    => '车架号应由字母和数字组成',
             'vin_code.size'         => '请输入17位车架号',
+            'plate_provence.min'    => '请输入车源所在身份',
+            'plate_city.min'        => '请输入车源所在城市',
 
         ];
     }
