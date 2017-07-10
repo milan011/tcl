47b21e2f174e42b4f3871bdae1ad5d3f49ba1392
wcgInTaoCheLe <milan015@sina.com>
2017-07-10 16:17:18 +0800

    每次都根据ip获取城市

--- app/Http/Controllers/Home/CommonController.php
+++ app/Http/Controllers/Home/CommonController.php
@@ -22,12 +22,12 @@ class CommonController extends Controller
         Request $request
     ) {
 
-        if(empty(Session('current_city')) || empty(Session('current_city_name'))){
+        // if(empty(Session('current_city')) || empty(Session('current_city_name'))){
             //若session没有当前城市id或名称,则根据客户ip获得所在城市并写入session
             $this->request = $request;
             $current_ip = $request->getClientIp(); //获取ip
             // dd($current_ip);
-            $current_ip = '106.117.13.179';
+            // $current_ip = '106.117.13.179';
             // $current_ip = '219.148.74.209';
             $city_info  = getCurrentCityByIp($current_ip); //根据ip获取城市信息
 
@@ -42,6 +42,6 @@ class CommonController extends Controller
                 session(['current_city' => '138']); 
                 session(['current_city_name' => '石家庄']);
             }
-        }
+        // }
     }
 }
