050ff2208ea7864cc4f479b323c0c8ab5553877c
wcgInTaoCheLe <milan015@sina.com>
2017-09-21 10:34:02 +0800

    基本js

--- public/newShow/js/common/common.js
+++ public/newShow/js/common/common.js
@@ -153,4 +153,29 @@
 
     $(init);
 
+    $.fn.hoverDelay = function(options) {
+            var defaults = {
+                hoverDuring: 1000,
+                outDuring: 1000,
+                hoverEvent: function() {
+                    $.noop();
+                },
+                outEvent: function() {
+                    $.noop();
+                }
+            };
+            var sets = $.extend(defaults, options || {});
+            var hoverTimer, outTimer;
+            return $(this).each(function() {
+                $(this).hover(function() {
+                    clearTimeout(outTimer);
+                    hoverTimer = setTimeout(sets.hoverEvent, sets.hoverDuring);
+                },
+                function() {
+                    clearTimeout(hoverTimer);
+                    outTimer = setTimeout(sets.outEvent, sets.outDuring);
+                });
+            });
+        }
+
 })(jQuery);
