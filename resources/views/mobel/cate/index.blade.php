782ee939c752a1da5891cf6fee3ba4447b719ec9
wcgInTaoCheLe <milan015@sina.com>
2017-10-13 10:22:10 +0800

    微信分享链接

--- resources/views/mobel/cate/index.blade.php
+++ resources/views/mobel/cate/index.blade.php
@@ -502,28 +502,26 @@
             // };
             // 2. 分享接口
             // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
-            setTimeout(function() {
-                // alert('已注册获取“发送给朋友”状态事件');
-                // 分享到朋友圈
-                wx.onMenuShareAppMessage({
-                    title: '淘车乐',
-                    desc: '卖车人的故事讲给买车人听',
-                    link: 'http://m.sjztcl.com',
-                    imgUrl: 'http://www.sjztcl.com/getheadimg.png',
-                    trigger: function (res) {
-                      // alert('用户点击发送给朋友');
-                    },
-                    success: function (res) {
-                      // alert('已分享');
-                    },
-                    cancel: function (res) {
-                      // alert('已取消');
-                    },
-                    fail: function (res) {
-                      // alert(JSON.stringify(res));
-                    }
-                });
-            }, 2000);
+            // alert('已注册获取“发送给朋友”状态事件');
+            // 分享到朋友圈
+            wx.onMenuShareAppMessage({
+                title: '淘车乐',
+                desc: '卖车人的故事讲给买车人听',
+                link: 'http://m.sjztcl.com',
+                imgUrl: 'http://www.sjztcl.com/getheadimg.png',
+                trigger: function (res) {
+                  // alert('用户点击发送给朋友');
+                },
+                success: function (res) {
+                  // alert('已分享');
+                },
+                cancel: function (res) {
+                  // alert('已取消');
+                },
+                fail: function (res) {
+                  // alert(JSON.stringify(res));
+                }
+            });
         // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
             wx.onMenuShareTimeline({
             title: '淘车乐',
